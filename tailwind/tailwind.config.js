1c8f471a7824e37007ff606dcdc9f607e8eaf77b
Min-Haw, Liu <liuminhaw@gmail.com>
2024-03-08 02:28:33 +0800

    refactor: tailwindcss config

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["../templates/**/*.{gohtml,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "satisfy": ["Satisfy"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
