d3ecb0064b3235d3164d998ee304e2377aa186f7
Min-Haw, Liu <liuminhaw@gmail.com>
2024-03-14 00:38:21 +0800

    feat: new migration sql repository_settings

---
+++ migrations/00008_repository_settings.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE repository_settings (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "snapshot_check_interval" INT NOT NULL,
+    "backup_status" VARCHAR(20) DEFAULT 'unknown' NOT NULL,
+    "recent_snapshot" TIMESTAMPTZ DEFAULT NULL,
+    "repository_id" INT NOT NULL,
+    CONSTRAINT fk_repositories
+        FOREIGN KEY ("repository_id") REFERENCES repositories(id)
+        ON DELETE CASCADE
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE repository_settings;
+-- +goose StatementEnd
